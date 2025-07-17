c91cc13c802bfc99b03f8066aa8ff46b9b133567
mannatgupta146 <mannatgupta146@gmail.com>
2025-07-17 23:51:38 +0530

    added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+// Wrap initialization in DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
 });
