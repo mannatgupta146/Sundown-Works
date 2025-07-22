5f839b6a3cb7492105029b9006096073c12db65f
mannatgupta146 <mannatgupta146@gmail.com>
2025-07-23 01:06:32 +0530

    added

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
         smooth: true
     });
 });
+
+var elemC = document.querySelector('#elem-container')
+var fixed = document.querySelector("#fixed-image")
+
+elemC.addEventListener('mouseenter', function() {
+    fixed.style.display = 'block';
+});
+elemC.addEventListener('mouseleave', function() {
+    fixed.style.display = 'none';
+});
