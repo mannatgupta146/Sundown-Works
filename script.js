593499e483b398fb00c9a7e7b40f6025df9cacbb
mannatgupta146 <mannatgupta146@gmail.com>
2025-07-27 00:03:07 +0530

    added

--- script.js
+++ script.js
@@ -77,3 +77,11 @@ function menuAnimation() {
     })
 }
 menuAnimation()
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+loaderAnimation()
